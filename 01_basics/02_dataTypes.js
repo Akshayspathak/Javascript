47bcadad394a900f4226fa7b0e2053d877b9112a
Akshay Pathak <93964429+Akshayspathak@users.noreply.github.com>
2024-03-27 09:50:30 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict"; // treat all JS code as newer version
+
+
+// alert (3+3) // we are using node.js not a browser
+
+
+// Data Types //
+
+// 1 number => 2 to power 53
+//bigint
+//string=> ""
+//boolean => true/false
+//null => standalone value (Khali aahe) empty
+//undefined => 
+//symbol => unique
+
+
+
+// object
+
+
+console.log(typeof undefined); //o/p undefined
+console.log(typeof null); // o/p object
