63bea308a389457c96e8f792b87d7b287727eeb0
Akshay Pathak <93964429+Akshayspathak@users.noreply.github.com>
2024-03-28 04:57:06 +0000

    Opration Done

--- 01_basics/03_conversionOpration.js
+++ 01_basics/03_conversionOpration.js
@@ -24,3 +24,25 @@ console.log(typeof setValue);
 // 1 => true, 0 => false
 // "" => false
 // "Aksh" => true
+
+
+// ******************** Opration ********************** //
+
+let value1 = 4
+let negativeValue = -value1
+
+// console.log(negativeValue);
+
+
+// console.log(2+2);
+// console.log(2**3);
+// console.log(2%3);
+
+
+let str1 = "aakash"
+let str2 = " more"
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
