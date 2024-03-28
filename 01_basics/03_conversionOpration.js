19f1d3ec197f677491713b5c1967e04106803bf6
Akshay Pathak <93964429+Akshayspathak@users.noreply.github.com>
2024-03-28 03:21:59 +0000

    typeof done

---
+++ 01_basics/03_conversionOpration.js
@@ -0,0 +1,26 @@
+let score = "33"
+
+console.log(typeof score);
+
+let valueNumber = Number(score)
+console.log(typeof valueNumber)
+
+// "33" => 33
+// "33abc" => NaN (Not a Number)
+// true => 1; false => 0;
+
+
+let isLoggedIn = 0
+
+let booleanIsLoggedIn= Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+let value = "42"
+
+let setValue = Number(value)
+console.log(setValue);
+console.log(typeof setValue);
+
+// 1 => true, 0 => false
+// "" => false
+// "Aksh" => true
