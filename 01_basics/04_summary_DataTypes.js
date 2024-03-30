269290b5c9875a11bfa86978b827f16608ba48a8
Akshay Pathak <93964429+Akshayspathak@users.noreply.github.com>
2024-03-30 03:30:42 +0000

    summary done

---
+++ 01_basics/04_summary_DataTypes.js
@@ -0,0 +1,24 @@
+ 
+// DataTypes vdo 9 //
+
+ //*********************************************************************************************
+
+
+
+
+// ************************ // Memory // *****************************************************
+
+// two types of Memory 
+
+// 1 Stack (primitive) with copy bhete  2 Heap (Non Promimitive) with refrance bheta
+
+let myYoutubename = "AAkashMoreyoutube.com"
+
+
+let otherName = myYoutubename
+
+otherName = "AkshayPathak.com"
+
+
+console.log(otherName)
+console.log(myYoutubename)
