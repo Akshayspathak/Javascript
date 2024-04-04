29c37fb14ec4389a073366e9800303d05bd24714
Akshay Pathak <93964429+Akshayspathak@users.noreply.github.com>
2024-04-04 12:12:10 +0000

    done

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,81 @@
+
+
+function myName() {
+
+    console.log("A");
+console.log("K");
+console.log("S");
+console.log("H");
+console.log("A");
+console.log("Y");
+    
+}
+
+// myName()
+
+function addTwoNumbers(number1,number2){
+    return number1 + number2
+
+}
+
+const result = addTwoNumbers(10,2)
+// console.log("result", result);
+
+function loginUser(username) {
+
+    return `${username} just login`
+
+    
+}
+// console.log(loginUser("Akshay"));
+
+
+
+
+function calculateCartPrice (val1,...num1) {
+return num1
+}
+// console.log(calculateCartPrice(3,5,9));
+
+
+
+
+const user= {
+name:"Hitesh",
+price:199
+}
+
+function handleObject (anyObject) {
+
+    console.log(`my name is ${anyObject.name} & my bot price is ${anyObject.price}`);
+
+}
+
+handleObject(user);
+
+// Array
+
+// const myNewArray = [200,300,500,800,100]
+function returnSecondValue(getArray){
+    return getArray[4]
+}
+
+// console.log(returnSecondValue(myNewArray));  Or
+console.log(returnSecondValue([200,300,500,100,58]));
+
+///////// function
+
+function myUser () {
+
+}
+
+////////// object
+
+const myObject = {
+    name: "akshay",
+    age:22
+
+}
+///////// Array
+
+const myArray = [1,2,3,4,5,6]
---
+++ 03_basics/02_scope.js
@@ -0,0 +1,45 @@
+let a =300                // Global scope
+if(true){
+    let a = 10            // block of scope
+    const b = 20
+    // console.log("inner" ,a);
+
+}
+
+// console.log(a);
+
+
+// *********** Nested Scope ******************
+ function one () {
+    const username = "Akshay"
+
+    function two() {
+        const website = "youtubes"
+        console.log(username);
+    }
+    // console.log(website);
+    two()
+ }
+
+//  one()
+
+
+if (true) {
+
+    const username = "Hitesh"
+    if (username === "Hitesh") {
+        const website = " youtube"
+        // console.log(username + website);
+    }
+    
+}
+
+//*************************************************************************************************** */
+
+
+// *************** interesting *******************************
+
+console.log(addOne(5))
+function addOne(num){
+    return num + 1
+}
