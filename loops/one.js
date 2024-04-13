0c490161fcdb2a257b209de2238a544c11b05429
Akshay Pathak <93964429+Akshayspathak@users.noreply.github.com>
2024-04-13 06:08:00 +0000

    done

--- loops/one.js
+++ loops/one.js
@@ -1,7 +1,25 @@
 // *************** for ***************
 
-for (let i = 0; i <= 15; i++) {
+for (let i = 5; i <= 15; i++) {
 
-    console.log(i);
+const element = i
+if (element == 8) {
+    // console.log("this is a best Number");
     
+}
+    // console.log(i);
+    
+} 
+
+
+for (let i = 0; i <= 10; i++) {
+    console.log(`Outer loops value : ${i}`);
+    for (let J = 0; J <= 10; J++) {
+        // console.log(`inner loop value: ${J} and inner loop value ${i}`);
+
+        // console.log(i + '*' + J + '=' + i+J);    // PADHE    
+        
+        
+        
+    }
 }
