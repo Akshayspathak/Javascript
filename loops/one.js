36015602ba2e51db70dc84a896f5872e7e05e479
Akshay Pathak <93964429+Akshayspathak@users.noreply.github.com>
2024-04-15 02:14:27 +0000

    loops

--- loops/one.js
+++ loops/one.js
@@ -13,7 +13,7 @@ if (element == 8) {
 
 
 for (let i = 0; i <= 10; i++) {
-    console.log(`Outer loops value : ${i}`);
+    // console.log(`Outer loops value : ${i}`);
     for (let J = 0; J <= 10; J++) {
         // console.log(`inner loop value: ${J} and inner loop value ${i}`);
 
@@ -23,3 +23,38 @@ for (let i = 0; i <= 10; i++) {
         
     }
 }
+
+
+
+//******** Array********************* */
+
+
+let myArray = ["Akshay","Adi","Motya", "Piya"]
+// console.log(myArray.length);
+
+for (let index = 0; index < myArray.length; index++) {
+    const element = myArray[index];
+    // console.log(element);
+    
+}
+
+
+//************* Break & Countinue  ***********************/
+
+for (let index = 1; index <= 20; index++) {
+    if (index == 6) {
+        console.log("Best value is 6");
+       break
+        
+    }
+  console.log(`my value is ${index}`);
+}
+
+// for (let index = 1; index <= 20; index++) {
+//     if (index == 6) {
+//         console.log("Best value is 6");
+//         continue
+        
+//     }
+//   console.log(`my value is ${index}`);
+// }
