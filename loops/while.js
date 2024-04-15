a860eabf771940ca0a63023b9afebf84710da8fe
Akshay Pathak <93964429+Akshayspathak@users.noreply.github.com>
2024-04-15 02:37:16 +0000

    While & Do-While

---
+++ loops/while.js
@@ -0,0 +1,23 @@
+//*********** While Loop **************** */
+let index = 1
+while (index <= 16) {
+// console.log(`This is value is ${index}`);
+index = index + 2
+    
+}
+
+let myArray = ["ak","Adi","AAku","Piyu"]
+let i = 0
+while (i < myArray.length) {
+    // console.log(`This is my Frend is ${myArray[i]}`);
+    i = i+1  // Or i++
+}
+
+// ************ Do While Loop *************************
+
+let score = 1
+
+do {
+    console.log(`this is score is ${score}`);
+    score++
+} while (score <= 11);
