df7fb00e5691e804c994c19179e4d44ea37a6f95
Akshay Pathak <93964429+Akshayspathak@users.noreply.github.com>
2024-04-16 05:53:23 +0000

    Map

---
+++ loops/map.js
@@ -0,0 +1,27 @@
+// const myNumbers = [1,2,3,4,5,6,7,8,9,10]
+
+// const newNumbers = myNumbers.map(  (num) => { return num + 10})      // map use
+// console.log(newNumbers);
+
+// const newNum2 = myNumbers.forEach (  (num2) => {               // forEach use
+//     console.log(num2);
+// })
+
+
+//******************* */ Chaining Method  ********************************
+
+const myNumbers = [1,2,3,4,5,6,7,8,9,10]
+
+const myNewNumbers = myNumbers.map(  () => {}) 
+
+
+
+
+
+
+
+
+
+
+
+
