e925d9f80f1342c58865c00146542619957d295d
Akshay Pathak <93964429+Akshayspathak@users.noreply.github.com>
2024-04-22 05:27:10 +0000

    let,const,var

---
+++ 01_basics/01_varibles.js
@@ -0,0 +1,18 @@
+//*******  Variable & Constant ******************//
+
+
+const accountId = 1234567
+let accountEmail = "akshaysk.com"
+var accountPassword = "123434234"
+accountCity = "pune"
+
+//  const accountId = 121212 // not allowed const in constant variable
+
+
+accountEmail = "ad@ad.com"
+accountPassword = "121212121212"
+accountCity = "mumbai"
+
+console.log(accountId);
+console.table([accountEmail,accountPassword,accountCity])
+
